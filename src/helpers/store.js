5047f1d149e187c53ac61e8a8d590bbba656e6e8
Jacinta <rosedubby@gmail.com>
2019-05-08 13:27:34 +0100

    fix babel runtime error

--- src/helpers/store.js
+++ src/helpers/store.js
@@ -2,13 +2,12 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const { NODE_ENV } = process.env;
 
 const store = createStore(
   rootReducer,
   compose(
     applyMiddleware(thunk),
-    window.devToolsExtension && NODE_ENV === 'development' ? window.devToolsExtension() : f => f
+
   )
 );
 window.store = store;
